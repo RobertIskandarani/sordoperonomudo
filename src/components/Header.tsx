40d7a3a50f7d61cc7a97bee99dbcea25cf21a688
Robert-Iskandarani <riskandarani@fi.uba.ar>
2024-03-21 19:13:03 -0300

    Feat: Header responsive terminado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@ import Sections from '../data/Sections.json';
 import HamburguerIcon from './icons/HamburguerIcon.tsx';
 import '../styles/styles.css';
 
+export const prerender = false;
+
 export default function Header() {
   const [openMenu, setOpenMenu] = useState(false);
 
@@ -26,13 +28,13 @@ export default function Header() {
         <nav
           className={`${
             openMenu ? 'overflow-visible' : 'overflow-hidden header-nav-hide'
-          } transition-all duration-500 fixed left-0 bottom-0 w-full rounded-t-xl bg-white px-2 py-2 shadow-lg lg:static lg:my-0 lg:flex lg:w-auto lg:rounded-full`}
+          } transition-all duration-500 fixed left-0 bottom-0 w-full rounded-t-xl bg-white px-2 py-2 shadow-lg-y-up lg:shadow-lg lg:static lg:my-0 lg:flex lg:w-auto lg:rounded-full`}
         >
           <ul className='flex flex-col justify-center items-center w-full lg:flex-row lg:items-center lg:justify-between gap-2'>
             {Sections.map((section) => (
               <li
                 onClick={handleClick}
-                className={`transition-all duration-300 w-full lg:w-auto rounded-xl lg:rounded-full bg-[var(--${section.color})] lg:bg-transparent text-base hover:bg-[var(--${section.color})] hover:font-medium hover:text-white cursor-pointer px-4 py-2 text-center`}
+                className={`transition-all duration-300 w-full lg:w-auto rounded-xl lg:rounded-full ${section.color} lg:bg-transparent text-base hover:font-medium hover:text-white cursor-pointer px-4 py-2 text-center`}
               >
                 <a href={`#${section.link}`}>{section.title}</a>
               </li>
