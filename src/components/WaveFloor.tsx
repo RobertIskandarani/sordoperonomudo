4a651095cca85904c02a4f13e02eea2976d8456c
Robert-Iskandarani <riskandarani@fi.uba.ar>
2024-04-21 18:05:35 -0300

    Feat: se agrega seccion "sobre mi"

--- src/components/WaveFloor.tsx
+++ src/components/WaveFloor.tsx
@@ -1,11 +1,26 @@
 interface Props {
   color: string;
+  type?: number;
+  shadow?: boolean;
+  className?: string;
 }
 
-export default function WaveFloor({ color }: Props) {
+export default function WaveFloor({
+  color,
+  type = 1,
+  shadow = true,
+  className = '',
+}: Props) {
   return (
-    <span className='w-full flex drop-shadow-[0px_-4px_2px_rgba(0,0,0,.25)]'>
-      <span id='wave' className={`h-20 w-full bg-[${color}]`}></span>
+    <span
+      className={`${className} w-full flex ${
+        shadow ? 'drop-shadow-[0px_-4px_2px_rgba(0,0,0,.25)]' : ''
+      }`}
+    >
+      <span
+        id={type === 1 ? 'wave' : 'wave-2'}
+        className={`h-20 w-full bg-[${color}]`}
+      ></span>
     </span>
   );
 }
